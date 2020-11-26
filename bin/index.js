e91277220805ac74df005b51edcc82397906415e
rocktimsaikia <srocktim61@gmail.com>
2020-11-26 19:29:24 +0530

    FEAT: add new exclude packages flag

--- bin/index.js
+++ bin/index.js
@@ -10,20 +10,20 @@ const {init, updatePackageJson} = require('./cli');
 
 const cli = meow(`
 		Usage:
-			$ syzer outdated
 			$ syzer update
 			$ syzer update --latest
 `,
 {
 	flags: {
-		outdated: {
-			type: 'boolean',
-			alias: 'o'
-		},
 		latest: {
 			type: 'boolean',
 			alias: 'l',
 			default: false
+		},
+		exclude: {
+			type: 'string',
+			alias: 'x',
+			isMultiple: true
 		},
 		version: {
 			type: 'boolean',
@@ -34,13 +34,17 @@ const cli = meow(`
 );
 
 (async () => {
-	const outdatedPackages = parsePackage();
+	let outdatedPackages = parsePackage();
 	const pathToJson = path.resolve(process.cwd(), 'package.json');
 
-	if (cli.flags.outdated) {
+	if (cli.input.length === 0) {
 		const boxenOutdatedPackages = init(outdatedPackages);
 		process.stdout.write(boxenOutdatedPackages);
 		process.exit(0);
+	}
+
+	if (cli.input[0] === 'update' && cli.flags.exclude !== undefined) {
+		outdatedPackages = outdatedPackages.filter(pkg => !cli.flags.exclude.includes(pkg.name));
 	}
 
 	if (cli.input[0] === 'update' && cli.flags.latest) {
@@ -49,7 +53,7 @@ const cli = meow(`
 	}
 
 	if (cli.input[0] === 'update' && !cli.flags.latest) {
-		const updatedJson = await updatePackageJson(outdatedPackages);
+		const updatedJson = await updatePackageJson(outdatedPackages, 'wanted');
 		intoStream(updatedJson).pipe(fs.createWriteStream(pathToJson));
 	}
 })();
--- test/bin.test.js
+++ test/bin.test.js
@@ -9,17 +9,21 @@ const del = require('del');
 process.chdir(path.join(__dirname, 'dir_'));
 
 describe('main --bin execution test', () => {
-	before(() => {
+	this.overWritePackageJson = () => {
 		const jsonData = fs.readFileSync('package-mock.json', 'utf8');
 		fs.writeFileSync('package.json', jsonData);
+	};
+
+	before(() => {
+		this.overWritePackageJson();
 	});
 
 	after(async () => {
 		await del('./package.json');
 	});
 
-	it('--outdated, option to check for the outdated packages', () => {
-		const data = shell.exec('node ../../bin/index.js -o', {silent: true});
+	it('check for the outdated packages', () => {
+		const data = shell.exec('node ../../bin/index.js', {silent: true});
 		assert.isTrue(data.code === 0);
 	});
 
@@ -37,5 +41,25 @@ describe('main --bin execution test', () => {
 		const jsonData = JSON.parse(fs.readFileSync('package.json', 'utf8'));
 		assert.equal(jsonData.dependencies['is-even'], '1.0.0');
 		assert.equal(jsonData.dependencies['is-odd'], '3.0.1');
+	});
+
+	it('update --exclude is-odd, option to exclude provided packages', () => {
+		this.overWritePackageJson();
+
+		const data = shell.exec('node ../../bin/index.js update --exclude is-odd', {silent: true});
+		assert.isTrue(data.code === 0);
+		const jsonData = JSON.parse(fs.readFileSync('package.json', 'utf8'));
+		assert.equal(jsonData.dependencies['is-even'], '0.1.2');
+		assert.equal(jsonData.dependencies['is-odd'], '3.0.0');
+	});
+
+	it('update --latest --exclude is-odd, option to exclude provided packages with --latest flag', () => {
+		this.overWritePackageJson();
+
+		const data = shell.exec('node ../../bin/index.js update --latest --exclude is-odd', {silent: true});
+		assert.isTrue(data.code === 0);
+		const jsonData = JSON.parse(fs.readFileSync('package.json', 'utf8'));
+		assert.equal(jsonData.dependencies['is-even'], '1.0.0');
+		assert.equal(jsonData.dependencies['is-odd'], '3.0.0');
 	});
 });
